dec1140eea25a819250f11a3cd935832e9496631
Ramses Solano Ramirez <ramsessr@outlook.com>
2026-06-13 07:13:32 -0500

    feat: integration button

--- resources/views/expenses/index.blade.php
+++ resources/views/expenses/index.blade.php
@@ -7,12 +7,11 @@
 
         <div class="sm:ml-auto flex items-center gap-2">
             <a href="{{ route('expenses.create') }}" class="inline-flex items-center gap-2 rounded-lg border border-blue-600
-             bg-transparent px-4 py-2 text-sm font-medium text-blue-600
              hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-blue-600/40
              disabled:opacity-50 disabled:pointer-events-none">
                 Add Expense
             </a>
-            <a href="{{ route('expenses.fromMail') }}" class="inline-flex items-center gap-2 rounded-lg border border-blue-600
+            <a id="auth-microsoft" class="inline-flex items-center gap-2 rounded-lg border border-blue-600
              bg-transparent px-4 py-2 text-sm font-medium text-blue-600
              hover:bg-blue-50 focus:outline-none focus:ring-2 focus:ring-blue-600/40
              disabled:opacity-50 disabled:pointer-events-none">
@@ -47,46 +46,46 @@
 
                         <tbody>
                             @foreach ($expenses as $expense)
-                                @if ($expense['descripcion'] != null)
-                                    <tr>
-                                        <td>
-                                            {{ $expense['valor'] }}
-                                        </td>
-                                        <td>
-                                            {{ $expense['descripcion'] }}
-                                        </td>
-                                        <td>
-                                            {{ $expense['fecha'] }}
-                                        </td>
-                                        <td class="px-4 py-2">
-                                            <div class="flex justify-center items-center gap-2">
-                                                <a href="{{ route('expenses.edit', $expense['id']) }}" class="inline-flex items-center gap-2 rounded-lg border border-yellow-600
+                            @if ($expense['descripcion'] != null)
+                            <tr>
+                                <td>
+                                    {{ $expense['valor'] }}
+                                </td>
+                                <td>
+                                    {{ $expense['descripcion'] }}
+                                </td>
+                                <td>
+                                    {{ $expense['fecha'] }}
+                                </td>
+                                <td class="px-4 py-2">
+                                    <div class="flex justify-center items-center gap-2">
+                                        <a href="{{ route('expenses.edit', $expense['id']) }}" class="inline-flex items-center gap-2 rounded-lg border border-yellow-600
                                              bg-transparent px-4 py-2 text-sm font-medium text-yellow-600
                                              hover:bg-yellow-50 focus:outline-none focus:ring-2 focus:ring-yellow-600/40
                                              disabled:opacity-50 disabled:pointer-events-none">
-                                                    Edit
-                                                </a>
-                                                <form action="{{ route('expenses.destroy', $expense['id']) }}"
-                                                    method="POST" style="display:inline;">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit"
-                                                        class="inline-flex items-center gap-2 rounded-lg border border-red-600
+                                            Edit
+                                        </a>
+                                        <form action="{{ route('expenses.destroy', $expense['id']) }}"
+                                            method="POST" style="display:inline;">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit"
+                                                class="inline-flex items-center gap-2 rounded-lg border border-red-600
                                              bg-transparent px-4 py-2 text-sm font-medium text-red-600
                                              hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-red-600/40
                                              disabled:opacity-50 disabled:pointer-events-none"
-                                                        onclick="return confirm('Are you sure you want to delete this expense?');">
-                                                        Delete
-                                                    </button>
-                                                </form>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endif
+                                                onclick="return confirm('Are you sure you want to delete this expense?');">
+                                                Delete
+                                            </button>
+                                        </form>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endif
                             @endforeach
                         </tbody>
                     </table>
-                    
+
                 </div>
 
 
@@ -106,13 +105,29 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
     <script>
-        $(function () {
+        $(function() {
             $('#expenses').DataTable({
                 pageLength: 20,
                 dom: 'Bfrtip',
                 buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
             });
         });
+
+        document.getElementById('auth-microsoft').addEventListener('click', async function() {
+
+            const datosUrl = {{ $urlAuthEmail }};
+
+            const ancho = 600;
+            const alto = 700;
+            const izquierda = (screen.width / 2) - (ancho / 2);
+            const arriba = (screen.height / 2) - (alto / 2);
+
+            const popup = window.open(
+                datosUrl.url,
+                'MicrosoftAuth',
+                `width=${ancho},height=${alto},top=${arriba},left=${izquierda}`
+            );
+        })
     </script>
 
 </x-app-layout>
